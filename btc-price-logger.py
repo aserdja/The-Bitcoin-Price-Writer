0d70602505e4916eb8e1952c2e3cbe05553985f4
andrii <andreyserdjuk@icloud.com>
2023-07-17 13:14:09 +0200

    Added configuration to connect to DB. Refactored the code. Made function to fill historical BTC-candles to DB (without date and time)

--- btc-price-logger.py
+++ btc-price-logger.py
@@ -1,33 +1,14 @@
-import time
-from binance_api_keys import api_key, secret_api_key
-from win32com.shell import shell, shellcon
-from binance import Client
-from datetime import datetime
 # aws
 from aws_access_keys import access_key, secret_access_key
 import boto3
 import os
 from db_settings import host, username_to_db, password_to_db
-from db_functions import create_and_choose_database, create_table_in_db, usedb
-import mysql.connector
+from db_functions import create_db_and_table, insert_data_to_db
 
-
-def write_btc_prices_to_file():
-    with open(shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, None, 0) + r'\btc-prices-history.txt', 'w') as file_to_write:
-        for price in binance_client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, f"1 Jan, {str(datetime.now().year)}"):
-            file_to_write.write(f'{price[1]}, {price[2]}, {price[3]}, {price[4]}' + '\n')
-
-
-def upload_file_to_s3bucket(name_of_bucket, name_of_directory):
-    file_to_upload = shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, None, 0) + r'\btc-prices-history.txt'
-    bucket_to_upload = str(name_of_bucket)
-    upload_file_key = str(name_of_directory) + 'btc-prices-history.txt'
-    aws_client.upload_file(file_to_upload, bucket_to_upload, upload_file_key)
 ############
 
-binance_client = Client(api_key, secret_api_key)
+
 aws_client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
 
-create_and_choose_database()
-usedb()
-create_table_in_db()
+create_db_and_table()
+insert_data_to_db()
--- db_functions.py
+++ db_functions.py
@@ -1,21 +1,30 @@
+import datetime
+from binance_api_keys import Client, binance_client
+from datetime import datetime
 from db_settings import db
 
-def create_and_choose_database():
+def create_db_and_table():
+    create_database()
+    usedb()
+    create_table_in_db()
+    truncate_table()
+
+def create_database():
     cursor = db.cursor()
     with cursor:
-        cursor.execute('CREATE DATABASE IF NOT EXISTS btc_prices_saver1;')
+        cursor.execute('CREATE DATABASE IF NOT EXISTS btc_price_saver;')
 
 def usedb():
     cursor = db.cursor()
     with cursor:
-        cursor.execute('use btc_prices_saver1;')
+        cursor.execute('use btc_price_saver;')
 
 def create_table_in_db():
     cursor = db.cursor()
     sql_query = '''
-                    create table if not exists btc_prices_history_1hour_interval (
+                    create table if not exists btc_prices_history_hour_interval (
                     id_history int auto_increment primary key,
-                    datetime_of_price datetime not null unique,
+                    datetime_of_price datetime not null,
                     open_price decimal(9,2) not null,
                     max_price decimal(9,2) not null,
                     min_price decimal(9,2) not null,
@@ -23,3 +32,17 @@ def create_table_in_db():
                     ) character set utf8;'''
     with cursor:
         cursor.execute(sql_query)
+
+def truncate_table():
+    cursor = db.cursor()
+    with cursor:
+        cursor.execute('truncate btc_prices_history_hour_interval;')
+
+def insert_data_to_db():
+    dayy = 0
+    for price in binance_client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, f"1 Jan, {str(datetime.now().year)}"):
+        sql_query = 'insert into btc_prices_history_hour_interval (datetime_of_price, open_price, max_price, min_price, close_price) values (%s, %s, %s, %s, %s);'
+        cursor = db.cursor()
+        with cursor:
+            cursor.execute(sql_query, (datetime.now(), price[1], price[2], price[3], price[4]))
+            db.commit()
