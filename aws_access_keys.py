ef1629dda91a2fd007bc76f65715be64c49b231f
Andrii Serdiuk <138502612+aserdja@users.noreply.github.com>
2023-07-21 14:46:05 +0200

    Update aws_access_keys.py

--- aws_access_keys.py
+++ aws_access_keys.py
@@ -1,5 +1,5 @@
-access_key = 'AKIAZNULNJTSHMW3EIKA'
-secret_access_key = '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'
+access_key = ''
+secret_access_key = ''
 
 import boto3
 aws_client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
